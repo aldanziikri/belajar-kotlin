86357d7b77fb45c397b229e2da32987952ac9e47
aldanziikri <aldanzikri23398@gmail.com>
2025-05-10 21:17:44 +0700

    belajar data type number

---
+++ src/DataTypeNumber.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    var byte: Byte = 124
+    var short: Short = 32_700
+    var int: Int = 2000000000
+    var long: Long = 91_200_000_000
+
+    var float: Float = 3.14f
+    var double: Double = 33.724545
+
+    println(byte);
+    println(short);
+    println(int);
+    println(long);
+
+    println(float);
+    println(double);
+
+}
