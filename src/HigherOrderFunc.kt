cd373827fe532d8c0bb5a15f4d75607b54fdf543
aldanziikri <aldanzikri23398@gmail.com>
2025-05-11 23:03:53 +0700

    anonymous function

--- src/HigherOrderFunc.kt
+++ src/HigherOrderFunc.kt
@@ -2,5 +2,8 @@ fun main() {
     fun hello(name:String, transformer: (String) -> String):String{
         return "hello ${transformer(name)}"
     }
-    println(hello("aldan", {value:String -> value.uppercase()}))
+//    println(hello("aldan", {value:String -> value.uppercase()}))
+    println(hello("aldan", fun(value):String{
+        return value.uppercase()
+    }))
 }
